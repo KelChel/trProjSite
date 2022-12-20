5dadf24d88cdb4d9bf0284df33ee7fca488d14e7
KelBoy <80912866+KeruBoy@users.noreply.github.com>
2022-12-20 22:41:07 +0300

    чет добавил

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,21 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import re
 
 # st.set_option('deprecation.showPyplotGlobalUse', False)
 
 st.title("Рекомендации")
 st.write("реки")
+
+VKdf = pd.read_excel('posts.xlsx', sheet_name='РГООИ_Надежда_')
+VKdf['only_date'] = VKdf['date'].dt.strftime('%Y-%m')
+VKdf['only_time'] = VKdf['date'].dt.strftime('%H')
+VKdf['weekday'] = VKdf['date'].dt.dayofweek
+months = pd.date_range('2022-01-03', '2022-09-01', freq='1M', normalize=True)
+months_to_analyse = [d.strftime('%Y-%m') for d in months]
+vk2022 = VKdf.loc[VKdf['only_date'].isin(months_to_analyse)]
+st.pyplot()
 
 st.header("Разработчики")
 st.write("""[Колеух Максим](https://vk.com/keruman) - \n
