67966097492899c57c33ecd54eb063b5cc9ba0ee
KelBoy <80912866+KeruBoy@users.noreply.github.com>
2022-12-20 23:19:41 +0300

    load images

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,17 +8,11 @@ import matplotlib.pyplot as plt
 st.title("Рекомендации")
 st.write("реки")
 
-VKdf = pd.read_excel('posts.xlsx', sheet_name='РГООИ_Надежда_')
-VKdf['only_date'] = VKdf['date'].dt.strftime('%Y-%m')
-VKdf['only_time'] = VKdf['date'].dt.strftime('%H')
-VKdf['weekday'] = VKdf['date'].dt.dayofweek
-months = pd.date_range('2022-01-03', '2022-09-01', freq='1M', normalize=True)
-months_to_analyse = [d.strftime('%Y-%m') for d in months]
-vk2022 = VKdf.loc[VKdf['only_date'].isin(months_to_analyse)]
-st.pyplot()
+st.image()
+
 
 st.header("Разработчики")
-st.write("""[Колеух Максим](https://vk.com/keruman) - \n
+st.write("""[Колеух Максим](https://vk.com/kelchel) - \n
 [Иван Гречкин](https://vk.com/yokore) - \n
 [Егор Мацко](https://vk.com/kitsunnet) - \n
 [Вадим Игнатов](https://vk.com/qbubble) - \n
