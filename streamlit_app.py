bdf2b45bb96fa1a2add4f08c2086de346b379bef
KelBoy <80912866+KeruBoy@users.noreply.github.com>
2022-12-20 16:52:27 +0300

    Main

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,17 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+st.title("Рекомендации")
+st.write("реки")
+
+st.header("Разработчики")
+st.write("""[Колеух Максим](https://vk.com/keruman) - \n
+[Иван Гречкин](https://vk.com/yokore) - \n
+[Егор Мацко](https://vk.com/kitsunnet) - \n
+[Вадим Игнатов](https://vk.com/qbubble) - \n
+[Николай Сергиенко](https://vk.com/waflyaaa) -
+""")
