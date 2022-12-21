4c35c1099c487229f74a1241d821875e10d12fd5
KelBoy71 <80912866+KelBoy71@users.noreply.github.com>
2022-12-21 14:49:04 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import re
 
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
@@ -17,7 +18,46 @@ VKdf['weekday'] = VKdf['date'].dt.dayofweek
 months = pd.date_range('2022-01-03', '2022-09-01', freq='1M', normalize=True)
 months_to_analyse = [d.strftime('%Y-%m') for d in months]
 vk2022 = VKdf.loc[VKdf['only_date'].isin(months_to_analyse)]
-st.dataframe(VKdf.describe())
+
+def find_topic(text): 
+    regular = r'#\S+'
+    compiled = re.compile(regular)
+    try: 
+        lst_topics = compiled.findall(text)
+    except BaseException: 
+        lst_topics = ['No topic']
+    return lst_topics
+vk2022['topics'] = vk2022.apply(lambda row : find_topic(row['text']), axis=1)
+
+def word_count(textik): 
+    lst = str(textik).split()
+    return len(lst)
+vk2022['text_length'] = vk2022.apply(lambda row : word_count(row['text']), axis=1)
+vk2022['count'] = 1
+
+vk2022_exploded = vk2022.explode('topics')
+
+vkdf = vk2022
+vkexp_df = vk2022_exploded
+
+def text_group_by_count(textt):
+    if textt < 50:
+        return 50
+    elif textt >= 50 and textt < 100:
+        return 100 
+    elif textt >= 100 and textt < 150:
+        return 150 
+    elif textt >= 150 and textt < 200:
+        return 200 
+    elif textt >= 200 and textt < 250:
+        return 250 
+    else:
+        return 300 
+    
+vkdf['text_length'] = vkdf.apply(lambda row : text_group_by_count(row['text_length']), axis=1)
+
+st.dataframe(vkdf.describe())
+
 
 
 st.header("Разработчики")
